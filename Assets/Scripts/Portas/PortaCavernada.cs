cc559e25a340ef0870e4760fc9a857b02c98ffb7
matbrgod <mcosta.barroso@gmail.com>
2025-10-08 01:43:15 -0300

    Milestone 03
    - 3 atos interconectados
    - Nave faltando
    - Animações faltando
    - Sprite de inimigo do futuro faltando

---
+++ Assets/Scripts/Portas/PortaCavernada.cs
@@ -0,0 +1,24 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortaCavernada : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Collider2D collision = Physics2D.OverlapCircle(transform.position, 0.1f,64);
+        if (collision != null)
+        {
+            SceneManager.LoadScene("Caverna");
+            Debug.Log("Collision detected with" + collision.gameObject.name);
+        }
+    }
+}
