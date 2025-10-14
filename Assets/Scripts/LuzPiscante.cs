6a849c137bb709b7f541baffc2de7180997c1679
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-10-14 14:33:02 -0300

    atualização do Luan
    -Robos terrestres andam pro lado e pro outro
    -Luz vermelha pisca
    -Canvas com Hud na câmera

---
+++ Assets/Scripts/LuzPiscante.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+
+public class LuzPiscante : MonoBehaviour
+{
+    public Light2D luz;
+    private bool aumentando = false;
+
+    void Start()
+    {
+        if (luz == null)
+            luz = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        if (!aumentando)
+        {
+            luz.intensity -= 0.5f * Time.deltaTime;
+            if (luz.intensity <= 0.0f)
+            {
+                luz.intensity = 0.0f;
+                aumentando = true;
+            }
+        }
+        else
+        {
+            luz.intensity += 0.5f * Time.deltaTime;
+            if (luz.intensity >= 0.6f)
+            {
+                luz.intensity = 0.6f;
+                aumentando = false;
+            }
+        }
+    }
+}
