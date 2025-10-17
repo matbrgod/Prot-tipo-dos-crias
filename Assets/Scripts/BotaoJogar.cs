0a607af11152be70e9d6fa1784bd04ba78894aa1
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-10-16 21:32:36 -0300

    Menu
    -Atualizada Cena de Menu
    -Botões que funcionam
    -Cena de Game Over adicionada a Gameplay e build

--- Assets/Scripts/BotaoJogar.cs
+++ Assets/Scripts/BotaoJogar.cs
@@ -4,21 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class BotaoJogar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Play()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.anyKeyDown)
-        {
-        
             SceneManager.LoadScene("Cenario 01");
-
-        }
-
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void Leave()
+    {
+        Application.Quit();
     }
 }
