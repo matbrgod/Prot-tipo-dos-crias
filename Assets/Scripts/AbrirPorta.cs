3062b38b8db5464f0186fe5fc18e4d16484334e4
rtogashh <renanttogashi@gmail.com>
2025-10-19 14:15:55 -0300

    diálogo, porta e lógica de quests adicionados

---
+++ Assets/Scripts/AbrirPorta.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AbrirPorta : MonoBehaviour
+{
+    public GameObject porta;
+    private Player player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter2D(Collider2D objectThatEntered)
+    {
+        if (objectThatEntered.CompareTag("Player"))
+        {
+            player = objectThatEntered.GetComponent<Player>();
+        }
+        Object.Destroy(porta);
+    }
+}
