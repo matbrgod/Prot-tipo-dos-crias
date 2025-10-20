674be4faff6ced7ca3e1c9fa150c5d286f0bc647
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-10-20 20:31:23 -0300

    Atualização do futuro
    -Iluminação
    -Assets e cenário do primeiro laboratório
    -Ia dos inimigos atiradores juntada no mesmo script

--- Assets/Scripts/EnemyAtirador.cs
+++ Assets/Scripts/EnemyAtirador.cs
@@ -11,12 +11,22 @@ public class EnemyAtirador : MonoBehaviour
     private float distance;
 
     private Rigidbody2D rb;
+    private bool atirar = false;
+    private bool mirar = false;
+    private float fireCooldown = 1;
+    private float fireTimer = 0f;
+    Vector2 moveDirection;
+    private Transform playerPosition;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireForce = 20f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         healthEnemy = maxHealthEnemy;
         player = GameObject.FindWithTag("Player");
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
     }
     void Update()
     {
@@ -26,20 +36,46 @@ public class EnemyAtirador : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
-        
+
 
         if (distance > distanceBetween)
         {
+            mirar = false;
+            atirar = false;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * (angle-180));
+            transform.rotation = Quaternion.Euler(Vector3.forward * (angle - 180));
+        }
+        if (distance <= distanceBetween)
+        {
+            mirar = true;
+            atirar = true;
+        }
+        
+        if (atirar == true)
+        {
+            fireTimer += Time.deltaTime;
+            if (fireTimer >= fireCooldown)
+            {
+                Atirar();
+                fireTimer = 0f;
+            }
         }
     }
 
     void FixedUpdate()
     {
-        
+        if (mirar)
+        {
+            Vector2 aimDirection = (Vector2)playerPosition.position - rb.position;
+            float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg -90f;
+            rb.rotation = aimAngle;
+        }
+    }
 
-
+    public void Atirar()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
     }
 
     public void TakeDamage(float damage)
@@ -55,14 +91,21 @@ public class EnemyAtirador : MonoBehaviour
     {
         if (collision.collider.CompareTag("Bullet"))
         {
-           healthEnemy -= 1;// Assuming each bullet reduces health by 1
-           if (healthEnemy <= 0)
+            healthEnemy -= 1;// Assuming each bullet reduces health by 1
+            if (healthEnemy <= 0)
             {
                 Destroy(gameObject);
             }
-
-
         }
     }
+    
+    /*void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.CompareTag("Player"))
+            {
+                atirar = true;
+                mirar = true;
+            }
+        }*/
 }
    
