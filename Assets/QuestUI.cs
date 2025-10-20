82cfd5141002b112f5a5b32a76763384701503d5
rtogashh <renanttogashi@gmail.com>
2025-10-19 22:53:58 -0300

    quests e inv

---
+++ Assets/QuestUI.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+public class QuestUI : MonoBehaviour
+{
+    public Transform questListContent;
+    public GameObject questEntryPrefab;
+    public GameObject objectiveTextPrefab;
+
+    public Quest testQuest;
+    public int testQuestAmount;
+    private List<QuestProgress> testQuests= new();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i = 0; i < testQuestAmount; i++)
+        {
+          testQuests.Add(new QuestProgress(testQuest));
+        }
+
+        UpdateQuestUI();
+    }
+
+    public void UpdateQuestUI()
+    {
+        foreach (Transform child in questListContent)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (var quest in testQuests)
+        {
+            GameObject entry = Instantiate(questEntryPrefab, questListContent);
+            TMP_Text questNameText = entry.transform.Find("QuestName").GetComponent<TMP_Text>();
+            Transform objectiveList = entry.transform.Find("ObjectiveList");
+
+            foreach (var objective in quest.objectives)
+            {
+                GameObject objTextGO = Instantiate(questEntryPrefab, objectiveList);
+                TMP_Text objText = objTextGO.GetComponent<TMP_Text>();
+                objText.text = $"{objective.description} ({objective.currentAmount}/{objective.requiredAmount})";
+
+                questNameText.text = quest.quest.name;
+            }
+        }
+    }
+}
--- Assets/Scripts/NpcDialogue.cs
+++ Assets/Scripts/NpcDialogue.cs
@@ -27,7 +27,7 @@ public class NpcDialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1") && readyToSpeak)
+        if(Input.GetKeyDown(KeyCode.E) && readyToSpeak)
         {
             if (!startDialogue)
             {
