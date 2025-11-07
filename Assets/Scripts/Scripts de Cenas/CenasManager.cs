216829a013fa3d2c71b5b62aed8d02531f6bf6b6
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-11-07 12:47:32 -0300

    Cenas Manager despausa
    CenasManager faz timeScale da cena voltar a 1 no começo da Cena

--- Assets/Scripts/Scripts de Cenas/CenasManager.cs
+++ Assets/Scripts/Scripts de Cenas/CenasManager.cs
@@ -6,6 +6,7 @@ public class CenasManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Time.timeScale = 1f; // Resume the game
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
         if (sceneName == "Menu")
