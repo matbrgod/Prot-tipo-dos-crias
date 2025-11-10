cb006af8eba3259228a1fa58d87f8d326c5b6230
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2025-11-10 03:16:11 -0300

    Game Over com SpawnPoint
    -Tela de Game Over agora te leva para último nível que o jogador entrou
    -Tamanho dos Botões da tela de Game Over aumentados (achei que tava muito pequeno)

--- Assets/Scripts/Scripts de Cenas/CenasManager.cs
+++ Assets/Scripts/Scripts de Cenas/CenasManager.cs
@@ -5,6 +5,7 @@ public class CenasManager : MonoBehaviour
 {
     //private string sceneName = "nada";
     public static CenasManager Instance;
+    public string ultimaCenaCarregada;
     private void Awake()
     {
         if (Instance != null)
@@ -14,7 +15,7 @@ public class CenasManager : MonoBehaviour
         else
         {
             Instance = this;
-            //DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject);
         }
     }
     void OnEnable()
@@ -35,52 +36,62 @@ public class CenasManager : MonoBehaviour
         switch (sceneName)
         {
             case "Menu":
-            {
+                {
+                ultimaCenaCarregada = "Menu";
                 MusicManager.Instance.PlayMusic("Menu");
                 break;
-            }
+                }
             case "tutorial nave":
-            {
+                {
+                ultimaCenaCarregada = "tutorial nave";
                 MusicManager.Instance.PlayMusic("CavernaTensa");
                 break;
-            }
+                }
             case "Cenario 01":
-            {
+                {
+                ultimaCenaCarregada = "Cenario 01";
                     MusicManager.Instance.PlayMusic("Cavernas");
                     break;
-            }
+                }
             case"Caverna_cenario 01":
                 {
+                    ultimaCenaCarregada = "Caverna_cenario 01";
                     MusicManager.Instance.PlayMusic("CavernaTensa");
                     break;
                 }
-            case "Clima2":
+            case "Cima2":
                 {
+                    ultimaCenaCarregada = "Cima2";
                     MusicManager.Instance.PlayMusic("Cavernas");
                     break;
                 }
             case "Caverna2":
                 {
+                    ultimaCenaCarregada = "Caverna2";
                     MusicManager.Instance.PlayMusic("CavernaTensa");
                     break;
                 }
             case "Futuro 1":
                 {
+                    ultimaCenaCarregada = "Futuro 1";
                     MusicManager.Instance.PlayMusic("MiniGame");
                     break;
                 }
             case "Futuro 2":
                 {
+                    ultimaCenaCarregada = "Futuro 2";
                     MusicManager.Instance.PlayMusic("Cavernas");
                     break;
                 }
-                case "Futuro3":
+                case "Futuro 3":
                 {
+                    ultimaCenaCarregada = "Futuro 3";
                     MusicManager.Instance.PlayMusic("CavernaTensa");
                     break;
                 }
                 case "Futuro 4":
                 {
+                    ultimaCenaCarregada = "Futuro 4";
                     MusicManager.Instance.PlayMusic("Menu");
                     break;
                 }
@@ -88,8 +99,13 @@ public class CenasManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void UltimaCenaCarregada()
     {
-        
+        print(ultimaCenaCarregada);
+        if(ultimaCenaCarregada == null)
+        {
+            ultimaCenaCarregada = "Menu";
+        }
+        LevelManager.Instance.LoadScene(ultimaCenaCarregada, "CrossFade");
     }
 }
