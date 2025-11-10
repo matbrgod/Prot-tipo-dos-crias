70f665a6a550dbdd84798dc09b546a3dead8fe4a
matbrgod <mcosta.barroso@gmail.com>
2025-11-10 14:43:39 -0300

    Inventario no mundo 1

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -11,8 +11,8 @@ public class MenuController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Tab))
+    {   
+        if(Input.GetKeyDown(KeyCode.Q))
         {
             menuCanvas.SetActive(!menuCanvas.activeSelf);
         }
